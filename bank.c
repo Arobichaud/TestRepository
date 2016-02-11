a2a9d69c120ab595a4dffc371ca882efd819c4db
Anthony Robichaud <tony.robi@hotmail.com>
2016-02-11 17:21:25 -0500

    Ajustement au programme.

--- bank.c
+++ bank.c
@@ -5,65 +5,65 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #define BUFFER 17
 
 //Affiche str et demande un nombre entre min et max!
 int getNbFromUser(char* str, int min, int max);
-void transaction(int accountNo, int operation, int amount);
+void transaction(int accountNo, int operation);
 
 int main(int argc, char** argv)
 {
   int accountNo;
   int operation;
-  int amount;
 
   while(getNbFromUser("Effectuer une transaction ?\n0 = Exit\n1 = Faire Transaction\n",0,1))
   {
+    //TODO: afficher la liste des comptes.
     accountNo = getNbFromUser("Sur quel compte ?\n0\n1\n2\n3\n4\n", 0,4);
     operation = getNbFromUser("Options:\n0 = Consulter\n1 = Retrait\n2 = Depot\n", 0,2);
-    amount = getNbFromUser("De quel montant ? (max 1000$)\n", 0,1000);
 
     //Processus enfant execute la commande
     if(!fork())
     {
-      transaction(accountNo, operation, amount);
+      transaction(accountNo, operation);
     }
+    wait(NULL);
   }
 
-  while(wait(NULL) > 0);
   return 0;
 }
 
-void transaction(int accountNo, int operation, int amount)
+void transaction(int accountNo, int operation)
 {
   int file;
-  lseek(file, 17*accountNo, SEEK_SET);
+  int amount = 0;
   
   if(operation == 1 || operation == 2)
   {
-   file = open("accounts.txt", O_RDWR);
-   if(lockf(file, F_TLOCK, 17) == -1)
+    amount = getNbFromUser("De quel montant entier?\n", 0,9999);
+    file = open("accounts.txt", O_RDWR);
+    lseek(file, BUFFER*accountNo, SEEK_SET);
+    if(lockf(file, F_TLOCK, BUFFER) == -1)
     {
       printf("Erreur compte no %d deja en utilisation\nTRANSACTION ANNULEE\n", accountNo);
       close(file);
       kill(getpid(), SIGTERM);
     }
-
+    amount++;
     //TODO RETRAIT OU DEPOT MONTANT
-
-
-    sleep(10);
-    lockf(file, F_ULOCK, BUFFER);
-
+   
+   lockf(file, F_ULOCK, BUFFER);
   }
   else
   {
-    char buffer[17];
-    file = open("accounts.txt", O_RDONLY;
-    read(file, buffer, 16); 
-    buffer[17] = 0;
-    printf("%s\n", buffer);
+    char stringBuffer[BUFFER];
+    file = open("accounts.txt", O_RDONLY);
+    lseek(file, (BUFFER+1)*accountNo, SEEK_SET);
+    read(file, stringBuffer, BUFFER); 
+    stringBuffer[BUFFER] = 0;
+    printf("%s\n", stringBuffer);
   }
 
   close(file);
